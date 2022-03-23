5f853f8e5ad9661efc076f77dffc03f3192b790b
Shankar Rajugiri <shankarrajgiri0@gmail.com>
2022-03-23 06:36:30 +0000

    FAQs app created

---
+++ src/components/Faqs/index.js
@@ -0,0 +1,25 @@
+// Write your code here.
+import {Component} from 'react'
+import FaqItem from '../FaqItem'
+import './index.css'
+
+class Faqs extends Component {
+  render() {
+    const {faqsList} = this.props
+
+    return (
+      <div className="app-container">
+        <div className="faqs-app-container">
+          <h1 className="heading">FAQs</h1>
+          <ul className="faqs-container">
+            {faqsList.map(eachFaq => (
+              <FaqItem key={eachFaq.id} faqDetails={eachFaq} />
+            ))}
+          </ul>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Faqs
